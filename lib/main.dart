d685f0589f47897598376c5ebd8012c6582619ea
FlutLab.io <>
2022-08-30 18:19:08 +0000

    (30/08) Início do aplicativo da Copa do Mundo 2022

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,100 @@ class MyHomePage extends StatelessWidget {
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      body: ListaEquipes(),
     );
+  }
+}
+
+class ListaEquipes extends StatelessWidget {
+  ListaEquipes({Key? key}) : super(key: key);
+
+  var equipes = [
+    {
+      "nome": "Argentina",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/pt/f/fc/230px-Afa_logo.svg.png",
+      "grupo": "C"
+    },
+    {
+      "nome": "Arábia Saudita",
+      "url_brasao": "https://upload.wikimedia.org/wikipedia/pt/0/01/SAFF.png",
+      "grupo": "C"
+    },
+    {
+      "nome": "Catar",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/pt/a/a9/Associa%C3%A7%C3%A3o_do_Qatar_de_Futebol.png",
+      "grupo": "A"
+    },
+    {
+      "nome": "Equador",
+      "url_brasao": "https://upload.wikimedia.org/wikipedia/pt/7/74/FEFecu.png",
+      "grupo": "A"
+    },
+    {
+      "nome": "Estados Unidos",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/commons/8/86/Crest_of_the_United_States_Soccer_Federation.png",
+      "grupo": "B"
+    },
+    {
+      "nome": "Inglaterra",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/pt/2/2e/England_crest_2009.svg.png",
+      "grupo": "B"
+    },
+    {
+      "nome": "Irã",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/pt/a/a6/Football_Federation_Islamic_Republic_of_Iran.png",
+      "grupo": "B"
+    },
+    {
+      "nome": "México",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/pt/b/b3/SeleccionMexico.png",
+      "grupo": "C"
+    },
+    {
+      "nome": "País de Gales",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/en/d/dc/Wales_national_football_team_logo.svg",
+      "grupo": "B"
+    },
+    {
+      "nome": "Países Baixos",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/pt/a/a1/Netherlands_national_football_team_logo_2017.png",
+      "grupo": "A"
+    },
+    {
+      "nome": "Polônia",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/commons/c/c9/Herb_Polski.svg",
+      "grupo": "C"
+    },
+    {
+      "nome": "Senegal",
+      "url_brasao":
+          "https://upload.wikimedia.org/wikipedia/pt/7/7c/FSenegalaiseF.png",
+      "grupo": "A"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: equipes.length,
+        itemBuilder: (context, i) {
+          var equipe = equipes[i];
+          //var brasao = CircleAvatar(
+          //    backgroundImage: NetworkImage(equipe["url_brasao"].toString()));
+          var brasao = Image.network(equipe["url_brasao"].toString());
+          return ListTile(
+              leading: brasao,
+              title: Text(equipe["nome"].toString()),
+              subtitle: Text("Grupo " + equipe["grupo"].toString()));
+        });
   }
 }
